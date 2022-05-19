de7df5fb3d359f0f252ca9168651d1fb607107fb
KelvinLeeder <kleeder77@outlook.com>
2022-05-19 13:40:30 -0500

    Se agrega el endpoind PUT para actualizar datos

--- app.js
+++ app.js
@@ -25,14 +25,23 @@ app.get('/v1/explorers/:id', (req, res) =>{
     res.status(200).json(explorer)
 })
 
-//Crerar un endpoint que se encargue de crear un explorer
+//Crear un endpoint que se encargue de crear un explorer
 app.post('/v1/explorers', (req, res) => {
     console.log(`Api Explorers POST request ${new Date()}`)
     const requestBody = req.body //Parametros de un cliente
     res.status(201).json({message: "Created"})
 })
 
+//Crear un endpoint que se encargue de actualizar un explorer
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parametros de un cliente
+    res.status(200).json({message: "Upadated"})
+})
+
 //con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port${port}`)
 })
+
